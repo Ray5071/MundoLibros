c059f1fd469f111cc3120c6096f34c86eba52eee
Ray5071 <56565486+Ray5071@users.noreply.github.com>
2020-01-24 23:30:11 -0400

    Inicio

---
+++ MundoLibros/MundoLibros/Infrastructure/InstanteLocator.cs
@@ -0,0 +1,17 @@
+﻿using MundoLibros.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundoLibros.Infrastructure
+{
+    public class InstanteLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanteLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
---
+++ MundoLibros/MundoLibros/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundoLibros.ViewModel
+{
+    public class MainViewModel
+    {
+        private static MainViewModel instance;
+
+        public MainViewModel()
+        {
+            instance = this;
+        }
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+            {
+                return new MainViewModel();
+            }
+            return instance;
+        }
+    }
+}
